b8daff17a84069188ae712750566dea3355043d9
NeverMore27 <uragan.uragan.59@mail.ru>
2017-10-09 12:59:51 +0300

    Update merge-3.cpp

--- merge-3.cpp
+++ merge-3.cpp
@@ -6,6 +6,7 @@ using namespace std;
 template <typename Iterator1, typename Iterator2>
 auto merge(Iterator1 first1, Iterator1 last1, Iterator1 first2, Iterator1 last2, Iterator2 output)->Iterator2
 {
+// all forward iterators
 	while ((first1<last1) || (first2<last2))
 	{
 		if (*first1 < *first2)
