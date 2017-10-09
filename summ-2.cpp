b739a492c87c9eb0b1ee3abf4cace3d1a56ae5cd
NeverMore27 <uragan.uragan.59@mail.ru>
2017-10-09 12:13:19 +0300

    Second

--- summ-2.cpp
+++ summ-2.cpp
@@ -8,7 +8,7 @@ using namespace std;
 template <typename Iterator, typename T>
 auto find_two_elements_with_sum(Iterator first, Iterator last, T c)->std::pair<bool, std::pair<Iterator, Iterator>>
 {
-	//first - Forwar
+	//first - Forward
 	//last - Bidirectional
 	std::pair<bool, std::pair<Iterator, Iterator>> p;
 	p.first = false;
