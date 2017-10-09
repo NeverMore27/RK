e6cc3da29ca6b8cab4e8f7084feca7ff0fb56ed7
NeverMore27 <uragan.uragan.59@mail.ru>
2017-10-09 11:46:37 +0300

    second

---
+++ summ-2.cpp
@@ -0,0 +1,29 @@
+
+#include <iostream> 
+#include <algorithm>
+#include <iterator>
+
+using namespace std;
+
+template <typename Iterator, typename T>
+auto find_two_elements_with_sum(Iterator first, Iterator last, T c)->std::pair<bool, std::pair<Iterator, Iterator>>
+{
+	std::pair<bool, std::pair<Iterator, Iterator>> p;
+	p.first = false;
+	p.second.first = nullptr;
+	p.second.second = nullptr;
+	if (std::distance(first, last) == 0) throw "?";
+	while (!p.first)
+	{
+		if (*first + *last < c) first++;
+		if (*first + *last > c) last--;
+		if (*first + *last == c)
+		{
+			p.first = true;
+			p.second.first = first;
+			p.second.second = last;
+		}
+		if (std::distance(first, last) == 0) break;
+	}
+
+}
